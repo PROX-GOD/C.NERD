50fb4e4dcda49cd4863f55ef6dfd2a3d86d35481
PROX-GOD <117366207+PROX-GOD@users.noreply.github.com>
2023-03-05 01:18:33 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-import run
+import os, platform
+os.system('git pull')
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import run64
+elif bit == '32bit':
+    import run
