3cf72ef6c693f84bed5714f0a93fbddd353be3a0
PROX-GOD <117366207+PROX-GOD@users.noreply.github.com>
2023-03-09 17:32:59 +0545

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-import qq
+import os, platform
+os.system('git pull')
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import qq
+elif bit == '32bit':
+    import qq
